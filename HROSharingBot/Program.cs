9f3a97820d9ce08ca8a9f73e87bfffa71a83ad19
Erik Moldtmann <erik@moldtmann.de>
2017-09-20 15:07:02 +0200

    Projektdateien hinzufügen.

---
+++ HROSharingBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HROSharingBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
