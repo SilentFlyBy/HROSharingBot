6277eb67fd944688cf21b9d34075ed70087dd76e
Erik Moldtmann <erik@moldtmann.de>
2017-09-24 00:15:09 +0200

    try again when cat api throws error

--- HROSharingBot/Commands/CatCommandRunner.cs
+++ HROSharingBot/Commands/CatCommandRunner.cs
@@ -6,18 +6,31 @@ namespace HROSharingBot.Commands
 {
     public class CatCommandRunner : ICommandRunner
     {
+        private int TryCounter { get; set; }
         public async Task Run(long chatId)
         {
-            const string apiUrl = "http://thecatapi.com/api/images/get";
-
-            using (var http = new HttpClient())
-            using (var contentStream = await http.GetStreamAsync(apiUrl))
-            using (var ms = new MemoryStream())
+            try
             {
-                contentStream.CopyTo(ms);
-                ms.Position = 0;
+                const string apiUrl = "http://thecatapi.com/api/images/get";
+            
+                using (var http = new HttpClient())
+                using (var contentStream = await http.GetStreamAsync(apiUrl))
+                using (var ms = new MemoryStream())
+                {
+                    contentStream.CopyTo(ms);
+                    ms.Position = 0;
 
-                await TelegramBot.SendImageMessage(chatId, "", ms);
+                    await TelegramBot.SendImageMessage(chatId, "", ms);
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                TryCounter++;
+                if (TryCounter < 5)
+                    await Run(chatId);
+
+                else
+                    throw;
             }
         }
     }
